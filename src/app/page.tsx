75482c4656effdf28f6cadd988e8b3d18b508456
= <rohitbhatwara1@gmail.com>
2023-10-26 23:17:01 +0530

    feat(theme-toggler): update theme toggler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+'use client'
+
+import { MoonIcon, SunIcon } from 'lucide-react'
+import { useTheme } from 'next-themes'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
+  const { theme, setTheme } = useTheme()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-2">
-      <div>Next Theme Generator</div>
-      <Button>shadcn/ui</Button>
+    <main className="mt-16 min-h-screen p-4">
+      <div className="flex items-center justify-end">
+        <Button
+          onClick={() => {
+            setTheme(theme === 'light' ? 'dark' : 'light')
+          }}
+        >
+          {theme === 'light' ? <MoonIcon className="mr-2 h-4 w-4" /> : <SunIcon className="mr-2 h-4 w-4" />}
+          <span>Toggle Theme</span>
+        </Button>
+      </div>
     </main>
   )
 }
